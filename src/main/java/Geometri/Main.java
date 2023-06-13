e71189e8bfd9c025b460424f0a40fc124f853d75
ElyhanSirait <136247696+ElyhanSirait@users.noreply.github.com>
2023-06-13 23:23:29 +0800

    Main.java

--- src/main/java/Geometri/Main.java
+++ src/main/java/Geometri/Main.java
@@ -1,11 +1,19 @@
+package Geometri;
 
 public class Main {
+
     public static void main(String[] args) {
-        Lingkaran obj = new Lingkaran(7);
+        Lingkaran lingkaran = new Lingkaran(6.0);
+      
+        System.out.println("Lingkaran : ");
+        System.out.println("Luas Lingkaran : "+ Lingkaran.luas);
+        System.out.println("Keliling Lingkaran : "+Lingkaran.keliling);
         
-        System.out.println("Luas Lingkaran adalah :" +obj.luas);
-        System.out.println("Keliling Lingkaran adalah :" +obj.keliling);
+        PersegiPanjang PersegiPanjang = new PersegiPanjang(8.0,9.0);
+        
+        System.out.println("Persegi Panjang : ");
+        System.out.println("Luas Persegi Panjang : "+ PersegiPanjang.luas);
+        System.out.println("Keliling Persegi Panjang : "+PersegiPanjang.keliling); 
+    }
         
     }
-    
-}
